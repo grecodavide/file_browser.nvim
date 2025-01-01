b574eea524473fbf2d30ddff34874e7da97a66c7
Davide Greco <grecodavide.work@proton.me>
2025-01-01 22:44:20 +0100

    chore: clean up

--- lua/file_browser/init.lua
+++ lua/file_browser/init.lua
@@ -114,10 +114,6 @@ local default_mappings = {
         callback = "delete",
         args = { true },
     },
-
-    -- ##############
-    -- ### prompt ###
-    -- ##############
     {
         mode = { "i", "n" },
         lhs = "<BS>",
@@ -134,68 +130,6 @@ local default_mappings = {
         callback = "copy_to_cwd",
     },
 }
-
----@type file_browser.Config
-M.opts = {
-    start_insert = true,
-    display_symlinks = true,
-    group_dirs = true,
-    width_scale = 0.92,
-    height_scale = 0.92,
-    preview_width = 0.4,
-    max_prompt_size = 0.6,
-
-    show_hidden = true,
-    show_links = true,
-
-    use_treesitter = true,
-
-    marked_icons = {
-        selected = {
-            text = "█",
-            hl = "MiniIconsYellow",
-        },
-        cut = {
-            text = "█",
-            hl = "MiniIconsRed",
-        },
-    },
-
-    debounce = 200,
-
-    use_default_mappings = true,
-
-    mappings = {},
-
-    respect_ignore = true,
-}
-
----@type file_browser.State
-local state
-
----Opens the main window
----@param cwd string?: The path to search into. Default to cwd
-M.open = function(cwd)
-    state = require("file_browser.state"):new(M.opts)
-    utils.save_options(state.options_to_restore)
-
-    if cwd == nil or cwd == "" then
-        cwd = vim.fn.getcwd()
-    else
-        cwd = vim.fn.expand(cwd)
-    end
-
-    if cwd:sub(#cwd) ~= "/" then
-        cwd = cwd .. "/"
-    end
-
-    state:focus()
-    state:cd(cwd, M.opts.start_insert)
-end
-
-M.get_state = function()
-    return state
-end
 
 local function contains(tbl, value)
     for _, e in ipairs(tbl) do
@@ -265,6 +199,67 @@ local function extend_mappings(already_seen)
     end
 end
 
+---@type file_browser.Config
+M.opts = {
+    start_insert = true,
+    display_symlinks = true,
+    group_dirs = true,
+    width_scale = 0.92,
+    height_scale = 0.92,
+    preview_width = 0.4,
+    max_prompt_size = 0.6,
+
+    show_hidden = true,
+    show_links = true,
+
+    use_treesitter = true,
+
+    marked_icons = {
+        selected = {
+            text = "█",
+            hl = "MiniIconsYellow",
+        },
+        cut = {
+            text = "█",
+            hl = "MiniIconsRed",
+        },
+    },
+
+    debounce = 200,
+
+    use_default_mappings = true,
+
+    mappings = {},
+
+    respect_ignore = true,
+}
+
+---@type file_browser.State
+local state
+
+---Opens the main window
+---@param cwd string?: The path to search into. Default to cwd
+M.open = function(cwd)
+    utils.save_options(state.options_to_restore)
+
+    if cwd == nil or cwd == "" then
+        cwd = vim.fn.getcwd()
+    else
+        cwd = vim.fn.expand(cwd)
+    end
+
+    if cwd:sub(#cwd) ~= "/" then
+        cwd = cwd .. "/"
+    end
+
+    state:focus()
+    state:cd(cwd, M.opts.start_insert)
+end
+
+M.get_state = function()
+    return state
+end
+
 ---Sets up the plugin. Must always be called once
 ---@param opts file_browser.Config
 M.setup = function(opts)
@@ -274,8 +269,9 @@ M.setup = function(opts)
         local already_seen = get_user_mappings()
         extend_mappings(already_seen)
     end
-
     set_up = true
+
+    state = require("file_browser.state"):new(M.opts)
 end
 
 return M
